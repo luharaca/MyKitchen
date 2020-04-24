d5f4eb365060a7cc7fe7f76654bbd23e124a6e55
haochuan <haochuanran@gmail.com>
2020-04-23 23:29:17 -0400

    Disable unit tests

--- src/test/java/com/app/mykitchen/MyKitchenApplicationTests.java
+++ src/test/java/com/app/mykitchen/MyKitchenApplicationTests.java
@@ -1,8 +1,10 @@
 package com.app.mykitchen;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+@Disabled
 @SpringBootTest
 class MyKitchenApplicationTests {
 
