931961a8680e8011a379cd7169352dae78c77925
haochuan <haochuanran@gmail.com>
2020-04-24 23:55:54 -0400

    Add user, role, and userRole relations

---
+++ src/main/java/com/app/mykitchen/domain/security/Role.java
@@ -0,0 +1,49 @@
+package com.app.mykitchen.domain.security;
+
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
+@Entity
+public class Role {
+
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	@Column(name="role_id",updatable=false, nullable=false)
+	private Long id;
+	private String name;
+	
+	@OneToMany(mappedBy="role", cascade=CascadeType.ALL, fetch=FetchType.LAZY)
+	private List<UserRole> userRoles;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public List<UserRole> getUserRoles() {
+		return userRoles;
+	}
+
+	public void setUserRoles(List<UserRole> userRoles) {
+		this.userRoles = userRoles;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+}
