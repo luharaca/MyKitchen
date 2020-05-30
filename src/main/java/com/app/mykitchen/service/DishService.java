40b38dcad4f5ced660d62814f16918528245f87a
haochuan <haochuanran@gmail.com>
2020-05-30 03:30:20 -0400

    Add menu page

--- src/main/java/com/app/mykitchen/service/DishService.java
+++ src/main/java/com/app/mykitchen/service/DishService.java
@@ -1,8 +1,12 @@
 package com.app.mykitchen.service;
 
+import java.util.List;
+
 import com.app.mykitchen.domain.Dish;
 
 public interface DishService {
 	
 	Dish createDish(Dish dish);
+	
+	List<Dish> findAllDishes();
 }
