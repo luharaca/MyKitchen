9d38b755b1b02d036314d282be4c3119b45eaed8
lucas <luharaca>
2021-01-26 23:13:03 -0500

    test failing

--- src/test/java/com/app/mykitchen/MyKitchenApplicationTests.java
+++ src/test/java/com/app/mykitchen/MyKitchenApplicationTests.java
@@ -1,15 +1,10 @@
 package com.app.mykitchen;
 
 import org.junit.jupiter.api.Disabled;
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @Disabled
 @SpringBootTest
 class MyKitchenApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
 
 }
